c974ee23ecd8d606318402dcc19a2f8794158b13
yexiaoyi123 <33075854+yexiaoyi123@users.noreply.github.com>
2017-11-12 17:35:43 +0800

    “message”

---
+++ app.js
@@ -0,0 +1,13 @@
+App({
+  globalData:{
+
+  },
+  onLaunch(){
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success:(response) =>{
+       Object.assign(this.globalData,response.data)
+      }
+    })
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+const app = getApp()
+
+Page({
+ data:{
+   slides:null
+ },
+ onLoad(){
+   this.setData({
+     slides:app.globalData.slides
+   })
+ }
+
+})
