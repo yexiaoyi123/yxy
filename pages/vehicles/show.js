0e83be124cfdc2877d930d5d733d1e3ae8f646d9
yexiaoyi123 <33075854+yexiaoyi123@users.noreply.github.com>
2017-11-12 20:11:06 +0800

    “message”

---
+++ pages/vehicles/show.js
@@ -0,0 +1,22 @@
+const app = getApp()
+
+Page({
+  data:{
+    entity:null
+  },
+  onLoad(){
+    const id = 3
+    const entity = app.globalData.vehicles.filter((item) => {
+      return item.id == id
+    })
+
+this.setData({
+  entity:entiey[0]
+})
+
+wx.setNavigationBarTitle({
+  title: this.data.entity.header
+})
+
+  }
+})
